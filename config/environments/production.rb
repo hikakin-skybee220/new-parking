f70e087b985017239e4a1bf9059a2fabb2cd58f8
HikaruShimada <hikarureihika@gmail.com>
2021-02-11 17:29:31 +0900

    edit production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,13 +114,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :user_name => "app200834352@heroku.com",
-  　　　　　　　　:password => "rebdu6co7580",
-      :domain => "heroku.com",
-      :address => "smtp.sendgrid.net",
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => "heroku.com",
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 
 end
